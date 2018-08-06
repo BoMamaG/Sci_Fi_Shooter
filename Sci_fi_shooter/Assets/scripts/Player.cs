5812a8b64918a79c2624ed685a90d8ca2c4d6c95
Remco Liqui lung <remco_506@hotmail.com>
2018-08-06 21:26:04 +0200

    shooter

---
+++ Sci_fi_shooter/Assets/scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    InputController inputController;
+
+	// Use this for initialization
+	void Start () {
+        inputController = GameManager.Instance.InputController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("Horizontal : " + inputController.Horizontal);
+        print("Mouse: " + inputController.MouseInput);
+		
+	}
+}
