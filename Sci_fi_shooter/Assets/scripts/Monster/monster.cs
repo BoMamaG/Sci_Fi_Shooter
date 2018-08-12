aea7ea9ce9e89867a94670e57538e9ba4e829390
Remco Liqui lung <remco_506@hotmail.com>
2018-08-13 00:05:20 +0200

    shooter

--- Sci_fi_shooter/Assets/scripts/Monster/monster.cs
+++ Sci_fi_shooter/Assets/scripts/Monster/monster.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class monster : MonoBehaviour {
     public GameObject player;
 
     private NavMeshAgent nav;
-
+    private string state = "idle";
+    private bool alive = true;
 	// Use this for initialization
 	void Start () {
-        nav = GetComponent<NavMeshAgent>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(alive)
+        {
+            if(state == "idle")
+            {
+                Vector3 randomPos = Random.insideUnitSphere & 20f;
+                NavMeshHit navHit;
+                NavMesh.SamplePosition(transform.position + randomPos, out navHit, 20f, NavMesh.AllAreas);
+            }
+        }
         nav.SetDestination(player.transform.position);
 	}
 }
